74cda0d49d7fbe1f08a3edb080ec0f01427d9874
Debjit <debjit.dhar@innodeed.com>
2022-06-06 12:57:27 +0530

    added new sw files

--- src/index.tsx
+++ src/index.tsx
@@ -15,8 +15,10 @@ import App from './App';
 import { AuthProvider } from './contexts/JWTContext';
 import { SettingsProvider } from './contexts/SettingsContext';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import store from './store';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <StrictMode>
@@ -40,7 +42,9 @@ ReactDOM.render(
 );
 
 // If you want to enable client cache, register instead.
-serviceWorker.register();
+//serviceWorker.register();
+serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
